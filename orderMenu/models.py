5b42e39019bae617b2167e1d0c08690e929b97ae
yangjingbo <741762540@qq.com>
2018-04-04 17:11:34 +0800

    add database and some models 2018-4-4 17:11

--- orderMenu/models.py
+++ orderMenu/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Food(models.Model):
+	name = models.CharField(max_length = 100)
+	desc = models.CharField(max_length = 200)
+	price = models.FloatField()
+
+	def __str__(self):
+		return self.name + ' '+ str(self.price)
